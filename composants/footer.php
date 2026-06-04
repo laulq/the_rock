a5a782cc2476b5c2eb272f08900bfde5cb4a70f0
laulq <lola.vignais24@gmail.com>
2026-06-04 15:06:26 +0200

    Delete composants directory

--- composants/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>© The Rock - Conçu 100% from scratch.</p>
-</footer>
--- composants/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once 'composants/param.inc.php';
-require_once 'src/Utilisateurs/Administrer.php';
-require_once 'src/Radios/Administrer.php';
-
-$utilisateurs = new Utilisateurs\Administrer(MYHOST, MYDB, MYUSER, MYPASS);
-$radios       = new Radios\Administrer(MYHOST, MYDB, MYUSER, MYPASS);

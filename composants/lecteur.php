63037d50f4b65b113c1681603ee3d22baed358c7
Yukialex-prog <yuki.alex49@gmail.com>
2026-06-11 10:17:20 +0200

    lecteur.php v0.1.0
    Premier test avec lecture audio (script js connecté)

--- composants/lecteur.php
+++ composants/lecteur.php
@@ -3,21 +3,20 @@
         <div id="lecteur-controles" class="lecteur-controles">
 
             <figure id="lecteur-infos" class="lecteur-infos">
-                <!-- Radio cliquable ? Pour accéder à ses infos, à une liste... Si oui, je change pour mettre + qu'une image -->
-                <img src="placeholder_radio_actuelle" alt="pochette radio actuelle">
+                <img src="placeholder_radio_actuelle" alt="pochette radio actuelle" id="lecteur-image">
                 <figcaption>
-                    <p class="lecteur-nom-radio">Placeholder nom radio</p>
-                    <p class="lecteur-nom-musique">Placeholder musique</p>
+                    <p class="lecteur-nom-radio" id="lecteur-nom-radio">Placeholder nom radio</p>
+                    <p class="lecteur-nom-musique" id="lecteur-nom-musique">Placeholder musique</p>
                 </figcaption>
             </figure>
 
             <div id="lecteur-controles-bouton" class="lecteur-controles-bouton">
                 <a id="bouton-play" class="bouton-play" href="#">
-                    <img src="placeholder_bouton" alt="play/pause">
+                    <img src="placeholder_bouton" alt="play/pause" id="bouton-play-img">
                 </a>
             </div>
 
-            <!-- Qu'est-ce qui va prendre la place de la barre de lecture ? -->
+            <audio id="lecteur-audio"></audio>
 
             <div id="lecteur-controles-volume" class="lecteur-controles-volume">
                 <a id="mute" class="lecteur-mute" href="#">
@@ -25,10 +24,12 @@
                 </a>
                 <div id="lecteur-volume" class="lecteur-volume">
                     <label for="volume" class="sr-only">Volume</label>
-                    <input type="range" id="volume" class="lecteur-volume-range" min="0" max="100" step="1">
+                    <input type="range" id="volume" class="lecteur-volume-range" min="0" max="100" step="1" value="80">
                 </div>
             </div>
 
         </div>
     </div>
 </section>
+
+<script src="scripts/lecteur.js"></script>
